89c41f615edcaa5f45db8a149499e63fc3b95754
mohammadRostamiorg <mohammad.jayant@gmail.com>
2021-12-16 22:54:31 +0330

    first commit

---
+++ src/components/Projects.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="projects">
+    <h2>پروژه ها</h2>
+    <div class="row">
+      <h4>password manager</h4>
+      <p class="project-about">
+        لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده
+        از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و
+        سطرآنچنان که لازم است
+      </p>
+      <span style="font-size: 1.4rem;margin-bottom:10px">لینک : </span>
+
+      <br>
+      <a style="color: #0000ff;text-decoration : none;" href="http://github.com">github</a>
+      
+    </div>
+    <div class="row">
+      <h4>Basketshop</h4>
+      <p class="project-about">
+        لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده
+        از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و
+        سطرآنچنان که لازم است
+      </p>
+      <span style="font-size: 1.4rem;margin-bottom:10px">لینک : </span>
+
+      <br>
+      <a style="color: #0000ff;text-decoration : none;" href="http://github.com">github</a>
+      
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  beforeCreate: function () {
+    document.body.className = "projects";
+  },
+};
+</script>
+
+<style>
+#nav-about-li a {
+  color: white !important;
+}
+</style>
---
+++ src/components/skills.vue
@@ -0,0 +1,100 @@
+<template>
+  <section id="skills">
+    <h2>مهارت های اصلی</h2>
+    <div id="main-skills-chart">
+      <div class="row">
+        <span class="skill-title"> HTML </span>
+        <span class="skill-chart-parent">
+          <span style="width: 90%" class="skill-chart-child"> </span>
+        </span>
+        <span class="skill-description">2 تا 4 سال</span>
+      </div>
+      <div class="row">
+        <span class="skill-title"> css </span>
+        <span class="skill-chart-parent">
+          <span style="width: 75%" class="skill-chart-child"> </span>
+        </span>
+        <span class="skill-description">2 تا 4 سال</span>
+      </div>
+      <div class="row">
+        <span class="skill-title"> javascript </span>
+        <span class="skill-chart-parent">
+          <span style="width: 40%" class="skill-chart-child"> </span>
+        </span>
+        <span class="skill-description">2 تا 4 سال</span>
+      </div>
+      <div class="row">
+        <span class="skill-title"> php </span>
+        <span class="skill-chart-parent">
+          <span style="width: 70%" class="skill-chart-child"> </span>
+        </span>
+        <span class="skill-description">2 تا 4 سال</span>
+      </div>
+      <div class="row">
+        <span class="skill-title"> laravel </span>
+        <span class="skill-chart-parent">
+          <span style="width: 30%" class="skill-chart-child"> </span>
+        </span>
+        <span class="skill-description">کمتر از 1 سال</span>
+      </div>
+    </div>
+
+    <hr />
+    <h2>مهارت های فرعی</h2>
+    <div id="sub-skills-chart">
+      <div class="row">
+        <span class="skill-title"> vuejs </span>
+        <span class="skill-chart-parent">
+          <span style="width: 15%" class="skill-chart-child"> </span>
+        </span>
+        <span class="skill-description">کمتر از 1 سال</span>
+      </div>
+      <div class="row">
+        <span class="skill-title"> bootstrap </span>
+        <span class="skill-chart-parent">
+          <span style="width: 65%" class="skill-chart-child"> </span>
+        </span>
+        <span class="skill-description">1 تا 2 سال</span>
+      </div>
+    </div>
+
+    <hr>
+    <h2>
+      زبان
+    </h2>
+    <div id="lang-skills-chart">
+
+      <div class="row">
+        <span class="skill-title"> ترکی </span>
+        <span class="skill-chart-parent">
+          <span style="width: 100%" class="skill-chart-child"> </span>
+        </span>
+        <span class="skill-description">زبان مادری</span>
+      </div>
+      <div class="row">
+        <span class="skill-title"> فارسی </span>
+        <span class="skill-chart-parent">
+          <span style="width: 100%" class="skill-chart-child"> </span>
+        </span>
+        <span class="skill-description">زبان ملی</span>
+      </div>
+      <div class="row">
+        <span class="skill-title"> انگلیسی </span>
+        <span class="skill-chart-parent">
+          <span style="width: 60%" class="skill-chart-child"> </span>
+        </span>
+        <span class="skill-description">متوسط</span>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  beforeCreate: function () {
+    document.body.className = "skills";
+  },
+};
+</script>
+
+<style></style>
